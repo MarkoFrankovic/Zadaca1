392e51bf0edb411b4be1e8ebc0660e4bba6e168f
Marko <mfrankovi@student.unipu.hr>
2022-03-21 11:07:43 +0100

    Zadaca

---
+++ zadatak/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">{{ commits }}</div>
+  <li v-for="item in commits" v-bind:key="item.sha">Commit{{ item.sha }}</li>
+    Commit <router-link to="/about">{{ imem.sha }}</router-link>
+</template>
+
+<script>
+let ovjekt = {
+name: "HomeView",
+data: function() {
+  return{
+    commits: [],
+
+  };
+
+},
+async mounted(){
+  let podatci = await fetch('https://api.github.com/repos/vuejs/vue/commits');
+  console.log(podatci);
+
+  let rezultati = await podatci.json();
+console.log(rezultati);
+
+for(let item of rezultati){
+console.log(rezultati.sha);
+}
+
+
+this.commits = rezultati;
+}
+
+
+
+}
+
+</script>
